fdcd9a3d580249a036f003baae12c6f43b3d82c1
Erin Shields <elshields92@gmail.com>
2020-04-19 02:27:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function johnLennonFacts(array) {
 function iLoveTheBeatles(n) {
   var array = [];
   do {
-  array.push("I love the Beatles")
+  array.push("I love the Beatles!")
   n += 1;
 } while (n < 15);
   return array
