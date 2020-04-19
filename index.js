2c7ba4903c5ca546eb2f8fd4d6dd4db3fde2c41f
Erin Shields <elshields92@gmail.com>
2020-04-19 02:20:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var n = 0; n < musicians.length; n++) {
+    array.push(`${musicians[n]} plays ${instruments[n]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(array) {
+  let n = 0;
+  var newArray = [];
+  while (n < array.length) {
+    newArray.push(`${array[n]}!!!`);
+  }
+  return newArray;
+}
